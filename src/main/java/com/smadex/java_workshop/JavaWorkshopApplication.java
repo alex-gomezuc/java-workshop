d07f7c79d95391cff60464142fde55e36521c5a0
Alex Gómez <alex.gomez@smadex.com>
2025-10-31 16:39:55 +0100

    Initial commit

---
+++ src/main/java/com/smadex/java_workshop/JavaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.smadex.java_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smadex/java_workshop/JavaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smadex.java_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
