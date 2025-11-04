8a104e1370e65e8538a5dc5ae1f0e2678de86585
Alex Gómez <alex.gomez@smadex.com>
2025-11-04 16:13:40 +0100

    4 - Web Application example

--- src/main/java/com/smadex/java_workshop/JavaWorkshopApplication.java
+++ src/main/java/com/smadex/java_workshop/JavaWorkshopApplication.java
@@ -2,12 +2,24 @@ package com.smadex.java_workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JavaWorkshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaWorkshopApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(JavaWorkshopApplication.class, args);
+  }
+
+  @RestController
+  public static class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+      return "Hello, Smadex team! From " + Thread.currentThread();
+    }
+
+  }
 
 }
